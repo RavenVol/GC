d3cf98a21e9b41ba9b775027db35094d48f809fe
RavenVol <r.volianiuk@gmail.com>
2019-11-21 11:31:49 +0200

    Updates

---
+++ precache-manifest.ceb14c15c68f3367af2bf69106862e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c9999cec2965ef6828c406b94bc815",
+    "url": "/GC/index.html"
+  },
+  {
+    "revision": "953aa98c69a8a714e416",
+    "url": "/GC/static/css/main.8b21593f.chunk.css"
+  },
+  {
+    "revision": "dafa12a7ebab42d8fe0f",
+    "url": "/GC/static/js/2.63374bc6.chunk.js"
+  },
+  {
+    "revision": "953aa98c69a8a714e416",
+    "url": "/GC/static/js/main.4bfab631.chunk.js"
+  },
+  {
+    "revision": "0940a8e57a47294635aa",
+    "url": "/GC/static/js/runtime~main.b6933f35.js"
+  }
+]);
